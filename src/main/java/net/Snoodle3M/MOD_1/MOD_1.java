abbd8a3854e05935b5f0e225f522ef14512f20b5
Snoodle3M <70383685+Snoodle3M@users.noreply.github.com>
2020-08-28 10:38:27 -0400

    Add files via upload

---
+++ src/main/java/net/Snoodle3M/MOD_1/MOD_1.java
@@ -0,0 +1,10 @@
+package net.Snoodle3M.MOD_1;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MOD_1 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
